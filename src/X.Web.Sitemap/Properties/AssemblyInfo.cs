c15e27ef15ed73571545bb6968049a54a6b27009
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2017-01-04 23:01:59 +0200

    Update project properties

--- src/X.Web.Sitemap/Properties/AssemblyInfo.cs
+++ src/X.Web.Sitemap/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("agi.net.ua")]
 [assembly: AssemblyProduct("X.Framework")]
-[assembly: AssemblyCopyright("agi.net.ua © 2003-2013")]
+[assembly: AssemblyCopyright("agi.net.ua © 2003-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("X.Web.Sitemap.Tests")]
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.5.2")]
-[assembly: AssemblyFileVersion("1.1.5.2")]
+[assembly: AssemblyVersion("1.5.0.*")]
+[assembly: AssemblyFileVersion("1.5.0.100")]
