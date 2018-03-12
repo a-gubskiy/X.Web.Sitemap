09f135fec4c9d85044df1f95c22360c59894b658
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2018-03-12 18:48:03 +0200

    Update tests

--- src/X.Web.Sitemap.Tests/UnitTests/SitemapGeneratorTests/GenerateSitemapsTests.cs
+++ src/X.Web.Sitemap.Tests/UnitTests/SitemapGeneratorTests/GenerateSitemapsTests.cs
@@ -1,67 +1,64 @@
-﻿using NUnit.Framework;
+﻿using NSubstitute;
+using NUnit.Framework;
 using System.Collections.Generic;
 using System.IO;
-using NSubstitute;
 
 namespace X.Web.Sitemap.Tests.UnitTests.SitemapGeneratorTests
 {
-    [TestFixture]
-    public class GenerateSitemapsTests
-    {
-        private SitemapGenerator _sitemapGenerator;
-        private ISerializedXmlSaver<List<Url>> _sitemapSerializerMock;
+	[TestFixture]
+	public class GenerateSitemapsTests
+	{
+		private SitemapGenerator _sitemapGenerator;
+		private ISerializedXmlSaver<List<Url>> _sitemapSerializer;
 
-        [SetUp]
-        public void SetUp()
-        {
-            _sitemapSerializerMock = Substitute.For<ISerializedXmlSaver<List<Url>>>();
-            _sitemapGenerator = new SitemapGenerator(_sitemapSerializerMock);
-        }
+		[SetUp]
+		public void SetUp()
+		{			
+			_sitemapSerializer = new SerializedXmlSaver<List<Url>>(new TestFileSystemWrapper());
+			_sitemapGenerator = new SitemapGenerator(_sitemapSerializer);
+		}
 
-        [Test]
-        public void It_Only_Saves_One_Sitemap_If_There_Are_Less_Than_50001_Urls()
-        {
-            //--arrange
-            var maxNumberOfUrlsForOneSitemap = SitemapGenerator.MaxNumberOfUrlsPerSitemap;
-            var urls = new List<Url>(maxNumberOfUrlsForOneSitemap);
-            for (var i = 0; i < maxNumberOfUrlsForOneSitemap; i++)
-            {
-                urls.Add(new Url());
-            }
+		[Test]
+		public void It_Only_Saves_One_Sitemap_If_There_Are_Less_Than_50001_Urls()
+		{
+			var filesCount = 4;
+			var recordsCount = (SitemapGenerator.MaxNumberOfUrlsPerSitemap * 3) + 5;
+			var urls = new List<Url>();
 
-            //--act
-            _sitemapGenerator.GenerateSitemaps(urls, new DirectoryInfo("x"), "file");
+			for (var i = 0; i < recordsCount; i++)
+			{
+				urls.Add(new Url());
+			}
 
-            //--assert
-            _sitemapSerializerMock
-                .Received(1)
-                .SerializeAndSave(Arg.Is<Sitemap>(x => x.Count == maxNumberOfUrlsForOneSitemap), Arg.Any<DirectoryInfo>(), Arg.Any<string>());
-        }
+			var result = _sitemapGenerator.GenerateSitemaps(urls, new DirectoryInfo("x"), "file");
 
-        [Test]
-        public void It_Saves_Two_Sitemaps_If_There_Are_More_Than_50000_Urls_But_Less_Than_100001_And_It_Names_The_Files_With_A_Three_Digit_Suffix_Incrementing_For_Each_One()
-        {
-            //--arrange
-            var enoughForTwoSitemaps = SitemapGenerator.MaxNumberOfUrlsPerSitemap + 1;
-            var urls = new List<Url>(enoughForTwoSitemaps);
-            for (var i = 0; i < enoughForTwoSitemaps; i++)
-            {
-                urls.Add(new Url());
-            }
-            var fileName = "file";
-            var directory = new DirectoryInfo("x");
+			Assert.AreEqual(filesCount, result.Count);
+		}
 
-            //--act
-            _sitemapGenerator.GenerateSitemaps(urls, directory, fileName);
+		[Test]
+		public void It_Saves_Two_Sitemaps_If_There_Are_More_Than_50000_Urls_But_Less_Than_100001_And_It_Names_The_Files_With_A_Three_Digit_Suffix_Incrementing_For_Each_One()
+		{
+			//--arrange
+			var enoughForTwoSitemaps = SitemapGenerator.MaxNumberOfUrlsPerSitemap + 1;
+			var urls = new List<Url>(enoughForTwoSitemaps);
+			for (var i = 0; i < enoughForTwoSitemaps; i++)
+			{
+				urls.Add(new Url());
+			}
+			var fileName = "file";
+			var directory = new DirectoryInfo("x");
 
-            //--assert
-            _sitemapSerializerMock
-                .Received(1)
-                .SerializeAndSave(Arg.Is<Sitemap>(x => x.Count == SitemapGenerator.MaxNumberOfUrlsPerSitemap), Arg.Is<DirectoryInfo>(x => x == directory), Arg.Is<string>(x => x == "file-001.xml"));
+			//--act
+			_sitemapGenerator.GenerateSitemaps(urls, directory, fileName);
 
-            _sitemapSerializerMock
-                 .Received(1)
-                .SerializeAndSave(Arg.Is<Sitemap>(x => x.Count == 1), Arg.Is<DirectoryInfo>(x => x == directory), Arg.Is<string>(x => x == "file-002.xml"));
-        }
-    }
+			//--assert
+			_sitemapSerializer
+				.Received(1)
+				.SerializeAndSave(Arg.Is<Sitemap>(x => x.Count == SitemapGenerator.MaxNumberOfUrlsPerSitemap), Arg.Is<DirectoryInfo>(x => x == directory), Arg.Is<string>(x => x == "file-001.xml"));
+
+			_sitemapSerializer
+				 .Received(1)
+				.SerializeAndSave(Arg.Is<Sitemap>(x => x.Count == 1), Arg.Is<DirectoryInfo>(x => x == directory), Arg.Is<string>(x => x == "file-002.xml"));
+		}
+	}
 }
