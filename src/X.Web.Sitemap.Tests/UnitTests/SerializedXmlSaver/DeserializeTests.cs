af97646e6d47a94e23b156793b539cc90ecbb9b6
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2019-07-27 17:11:24 +0300

    Tests added. Close #15

---
+++ src/X.Web.Sitemap.Tests/UnitTests/SerializedXmlSaver/DeserializeTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace X.Web.Sitemap.Tests.UnitTests.SerializedXmlSaver
+{
+	[TestFixture]
+	public class DeserializeTests
+	{
+		[SetUp]
+		public void SetUp()
+		{
+			
+		}
+
+		[Test]
+		public void Check_That_XmlFile_Deserialized()
+		{
+			//--arrange
+
+			var xml = File.ReadAllText("Data/example.xml");;
+			var sitemap = Sitemap.Parse(xml);
+			
+			//--act
+			Assert.NotNull(sitemap);
+		}
+	}
+}
