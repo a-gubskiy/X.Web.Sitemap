fbe1fa7b60fd48d945846ccf70eaf9d1a5f018af
Andrew <andrew.gubskiy@outlook.com>
2023-03-02 18:36:15 +0200

    Rename SerializedXmlSaver to SitemapXmlSaver

---
+++ src/X.Web.Sitemap/SitemapXmlSaver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace X.Web.Sitemap;
+
+internal interface ISitemapXmlSaver
+{
+    FileInfo SerializeAndSave(Sitemap sitemap, DirectoryInfo targetDirectory, string targetFileName);
+}
+
+internal class SitemapXmlSaver : ISitemapXmlSaver
+{
+    private readonly IFileSystemWrapper _fileSystemWrapper;
+    private readonly SitemapSerializer _serializer;
+
+    public SitemapXmlSaver(IFileSystemWrapper fileSystemWrapper)
+    {
+        _serializer = new SitemapSerializer();
+        _fileSystemWrapper = fileSystemWrapper;
+    }
+
+    public FileInfo SerializeAndSave(Sitemap sitemap, DirectoryInfo targetDirectory, string targetFileName)
+    {
+        if (sitemap == null)
+        {
+            throw new ArgumentNullException(nameof(sitemap));
+        }
+        
+        var xml = _serializer.Serialize(sitemap);
+        var path = Path.Combine(targetDirectory.FullName, targetFileName);
+                
+        return _fileSystemWrapper.WriteFile(xml, path);
+    }
+}
