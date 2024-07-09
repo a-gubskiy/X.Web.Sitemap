0091bd2073700e66ec8d760ba149cf5c1ad6aeff
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-07-09 22:28:27 +0300

    Updat packages.

--- src/X.Web.Sitemap/FileSystemWrapper.cs
+++ src/X.Web.Sitemap/FileSystemWrapper.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 
 namespace X.Web.Sitemap;
 
+[PublicAPI]
 internal interface IFileSystemWrapper
 {
     FileInfo WriteFile(string xml, string path);
